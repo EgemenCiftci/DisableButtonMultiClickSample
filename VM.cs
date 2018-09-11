a22d1df9c82681567bf87f73decadbf67586a3ec
Egemen Çiftci <egemen.ciftci@icrontech.com>
2018-09-11 17:28:40 +0300

    Initial commit

---
+++ VM.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+
+namespace DisableButtonMultiClickSample
+{
+    public class VM
+    {
+        public virtual int ClickCount { get; set; }
+
+        public void Click()
+        {
+            ClickCount++;
+        }
+
+        public void PreviewMouseDoubleClick(MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
